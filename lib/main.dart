04e18352a1ab65b44df569fff94ce349bf1051f4
Kléber de Oliveira Andrade <pdjkleber@gmail.com>
2019-08-25 20:40:18 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Home(),
-      debugShowCheckedModeBanner: false,
-    ));
+void main() => runApp(
+      MaterialApp(
+        home: Home(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
 
 class Home extends StatefulWidget {
   @override
@@ -11,10 +13,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  TextEditingController _weightController = TextEditingController();
-  TextEditingController _heightController = TextEditingController();
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
+  TextEditingController _weightController = TextEditingController();
+  TextEditingController _heightController = TextEditingController();
   String _result;
 
   @override
@@ -31,6 +33,51 @@ class _HomeState extends State<Home> {
     });
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: buildAppBar(),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+            padding: EdgeInsets.all(20.0), child: buildForm()));
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      title: Text('Calculadora de IMC'),
+      backgroundColor: Colors.blue,
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.refresh),
+          onPressed: () {
+            resetFields();
+          },
+        )
+      ],
+    );
+  }
+
+  Form buildForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          buildTextFormField(
+              label: "Peso (kg)",
+              error: "Insira seu peso!",
+              controller: _weightController),
+          buildTextFormField(
+              label: "Altura (cm)",
+              error: "Insira uma altura!",
+              controller: _heightController),
+          buildTextResult(),
+          buildCalculateButton(),
+        ],
+      ),
+    );
+  }
+
   void calculateImc() {
     double weight = double.parse(_weightController.text);
     double height = double.parse(_heightController.text) / 100.0;
@@ -40,77 +87,52 @@ class _HomeState extends State<Home> {
       _result = "IMC = ${imc.toStringAsPrecision(2)}\n";
       if (imc < 18.6)
         _result += "Abaixo do peso";
-      else if (imc < 25)
+      else if (imc < 25.0)
         _result += "Peso ideal";
-      else if (imc < 30)
+      else if (imc < 30.0)
         _result += "Levemente acima do peso";
-      else if (imc < 35)
+      else if (imc < 35.0)
         _result += "Obesidade Grau I";
-      else if (imc < 40)
+      else if (imc < 40.0)
         _result += "Obesidade Grau II";
       else
         _result += "Obesidade Grau IIII";
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Calculadora de IMC'),
-          backgroundColor: Colors.blue,
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.refresh),
-              onPressed: () {
-                resetFields();
-              },
-            )
-          ],
-        ),
-        backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-            padding: EdgeInsets.all(20.0),
-            child: Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    TextFormField(
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(labelText: 'Peso (kg)'),
-                      controller: _weightController,
-                      validator: (text) {
-                        return text.isEmpty ? "Insira seu peso!" : null;
-                      },
-                    ),
-                    TextFormField(
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(labelText: 'Altura (cm)'),
-                      controller: _heightController,
-                      validator: (text) {
-                        return text.isEmpty ? "Insira sua altura!" : null;
-                      },
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 36.0),
-                      child: Text(_result, textAlign: TextAlign.center,),
-                    ),
-                    Padding(
-                        padding: EdgeInsets.symmetric(vertical: 36.0),
-                        child: Container(
-                            height: 50,
-                            child: RaisedButton(
-                              color: Colors.blueAccent,
-                              onPressed: () {
-                                if (_formKey.currentState.validate()) {
-                                  calculateImc();
-                                }
-                              },
-                              child: Text('CALCULAR', style: TextStyle(color: Colors.white)),
-                            ))),
-                            
-                  ],
-                ))));
+  Widget buildCalculateButton() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 36.0),
+      child: RaisedButton(
+        onPressed: () {
+          if (_formKey.currentState.validate()) {
+            calculateImc();
+          }
+        },
+        child: Text('CALCULAR', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+
+  Widget buildTextResult() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 36.0),
+      child: Text(
+        _result,
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  Widget buildTextFormField(
+      {TextEditingController controller, String error, String label}) {
+    return TextFormField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(labelText: label),
+      controller: controller,
+      validator: (text) {
+        return text.isEmpty ? error : null;
+      },
+    );
   }
 }
